0c42a56817d0691e0504c613d7cd4b5ba6bd18ab
crazy_dev <pelumidevelops18@gmail.com>
2026-06-08 00:12:22 +0100

    setup sessions postgres

--- internal/domain/repositories/session_store.go
+++ internal/domain/repositories/session_store.go
@@ -1,12 +1,10 @@
 package repositories
 
 import (
-	"github.com/google/uuid"
 	"github.com/pelDev/health-connect/internal/domain/models"
 )
 
 type SessionStorage interface {
-	CreateSession(sessionID uuid.UUID, vid uuid.UUID, ref *string) (*models.Session, error)
-	GetSession(sessionID uuid.UUID) (*models.Session, error)
+	Storage[models.Session]
 	GetSessionByReference(reference string) (*models.Session, error)
 }
